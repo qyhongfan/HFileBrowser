ce04d3d47e9c3aa5fbaf6b6e8e49195fe20e6c71
hurry.qin <hurry.qin@dji.com>
2017-03-06 15:34:52 +0800

    Initial Commit

---
+++ HFileBrowser/HFileBrowser.h
@@ -0,0 +1,19 @@
+//
+//  HFileBrowser.h
+//  HFileBrowser
+//
+//  Created by hurry.qin on 2017/3/6.
+//  Copyright © 2017年 hurry.qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HFileBrowser.
+FOUNDATION_EXPORT double HFileBrowserVersionNumber;
+
+//! Project version string for HFileBrowser.
+FOUNDATION_EXPORT const unsigned char HFileBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HFileBrowser/PublicHeader.h>
+
+
